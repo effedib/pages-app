0fc77df4f351f0c53026604c4126f92c2ac0fc95
Francesco <cicciodb@hotmail.it>
2024-06-19 23:35:33 +0200

    new updates for pythonanywhere deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8n5urgpu_fqeq0lq+dkwta=i5+v(bojr=w1+j4$&-2yn)fne-w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
